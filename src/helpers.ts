347693cbf26a583baabcd708dc67be6bd8953835
Sami Jaber <jabersami@gmail.com>
2018-06-08 17:28:09 -0400

    check for undefined instead of boolean coercion in getDisplayName

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,7 +1,7 @@
 import { ComponentType } from 'react';
 
 export const getDisplayName = (ComposedComponent: ComponentType<any>) =>
-  ComposedComponent.displayName || 'Component';
+  ComposedComponent.displayName !== undefined ? ComposedComponent.displayName : 'Component';
 
 export type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
 export type ObjectOmit<T extends K, K> = Omit<T, keyof K>;
